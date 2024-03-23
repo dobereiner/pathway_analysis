fdb267458c469f2d7e542cbc32c4a570021d660c
dobereiner <94486570+dobereiner@users.noreply.github.com>
2024-03-23 19:30:38 +0300

    Update core.py

--- core.py
+++ core.py
@@ -6,22 +6,28 @@ import seaborn as sns
 
 class PathwayDatabase:
     DATABASES = {
-        'hallmark': 'h.all.v2023.2.Hs.symbols.gmt',
-        'canonical pathways': 'c2.cp.v2023.2.Hs.symbols.gmt',
-        'reactome': 'c2.cp.reactome.v2023.2.Hs.symbols.gmt',
-        'transcription factor targets': 'c3.tft.v2023.2.Hs.symbols.gmt'
+        'hallmark': ['h.all.v2023.2.Hs.symbols.gmt', 'hallmark.csv'],
+        'canonical pathways': ['c2.cp.v2023.2.Hs.symbols.gmt', 'cp.csv'],
+        'reactome': ['c2.cp.reactome.v2023.2.Hs.symbols.gmt', 'reactome.csv'],
+        'transcription factor targets': ['c3.tft.v2023.2.Hs.symbols.gmt', 'tf.csv'],
+        'GOBP': ['c5.go.bp.v2023.2.Hs.symbols.gmt', 'go_bp.csv'],
+        'GOCC': ['c5.go.cc.v2023.2.Hs.symbols.gmt', 'go_cc.csv'],
+        'GOMF': ['c5.go.mf.v2023.2.Hs.symbols.gmt', 'go_mf.csv']
     }
 
-    def __init__(self, database: str, pathway: str=None, genes: List[str]=None) -> None:
+    def __init__(self, organ: str, database: str, pathway: str=None, genes: List[str]=None) -> None:
         self.pathways = {}
+        self.organ = organ
         
         if database != 'custom':
-            with open(f'./biopathways/databases/{self.DATABASES[database]}') as file:
+            with open(f'./pathway_analysis/databases/{self.DATABASES[database][0]}') as file:
                 for line in file:
                     pathway, _, *genes = line.removesuffix('\n').split('\t')
                     self.pathways[pathway] = genes
         else:
             self.pathways[pathway] = genes
+
+        self.data = pd.read_csv(f'./pathway_analysis/{self.organ}/{self.DATABASES[database][1]}', index_col=0)
 
     def __getitem__(self, pathway: str) -> List[str]:
         if pathway in self.pathways:
@@ -31,30 +37,7 @@ class PathwayDatabase:
         return [pathway for pathway in self.pathways if query in pathway]
 
 
-def read(filepath: str) -> pd.DataFrame:
-    match filepath.split('.')[-1]:
-        case 'csv':
-            sep = ','
-        case 'tsv':
-            sep = '\t'
-        case _:
-            raise Exception
-        
-    return pd.read_csv(filepath, sep=sep, index_col=0)
-
-
-def significance(p_value: float) -> str:
-    if p_value < 0.001:
-        return '***'
-    elif p_value < 0.01:
-        return '**'
-    elif p_value < 0.05:
-        return '*'
-    else:
-        return ''
-
-
-def heatmap(data: pd.DataFrame, annot: pd.DataFrame, method: str, metric: str):
+def heatmap(data: pd.DataFrame, method: str, metric: str):
     n_rows, n_cols = data.shape
 
     return sns.clustermap(data=data,
@@ -63,6 +46,5 @@ def heatmap(data: pd.DataFrame, annot: pd.DataFrame, method: str, metric: str):
                           figsize=(n_cols * 2, n_rows * 0.5),
                           cmap='bwr',
                           center=0,
-                          annot=annot.applymap(significance),
                           fmt='',
                           col_cluster=False)
